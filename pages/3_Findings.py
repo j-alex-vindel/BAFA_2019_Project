269f8cbe63f6d2f45c003f0bc36237e3538a9f31
Jose Alexander Vindel Garduno <60977557+j-alex-vindel@users.noreply.github.com>
2022-07-24 17:07:22 +0100

    Update 3_Findings.py

--- pages/3_Findings.py
+++ pages/3_Findings.py
@@ -34,7 +34,7 @@ F_General, F_Division,F_Team = st.tabs(['General Findings','Division','Team'])
 with F_General:
 
     with st.expander('About Scoring'):
-        if st.checkbox('Show Grap(h)s',key='Score'):
+        if st.checkbox('Show Graph(s)',key='Score'):
             col_a, col_b = st.columns([2,2])
             with col_a:
                 st.subheader('- Score')
