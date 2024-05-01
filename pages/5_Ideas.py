ed9cec86adc9e1e75a720f6d9363afd634b5d658
J. Alexander Vindel <60977557+j-alex-vindel@users.noreply.github.com>
2024-05-01 08:33:48 +0100

    Update and rename 5_Models.py to 5_Ideas.py

---
+++ pages/5_Ideas.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Predictive Model",
+    page_icon=":chart_with_upwards_trend:"
+)
+
+st.markdown('## Some ideas!')
+st.markdown(''' 
+ - ### Demographics
+     - #### Population Size
+     - #### Percentage of Men
+     - #### Income
+     - #### Number of youth programs
+
+''')
+            
--- pages/5_Models.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-st.set_page_config(
-    page_title="Predictive Model",
-    page_icon=":chart_with_upwards_trend:"
-)
-
-st.markdown('## To be Completed!')
